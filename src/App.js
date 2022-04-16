5bf92787e62b4dada74899a882d24ada5cf5cab5
Thomas K <thmskffr@gmail.com>
2022-04-16 13:16:15 +0200

    solved challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+import QuoteCard from "./components/QuoteCard";
+import { useState } from "react";
+
+const startQuote = [
+  {
+    quote: "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
+    character: "Homer Simpson",
+    image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+    characterDirection: "Right"
+  }
+];
 
 function App() {
-  return (
+
+  const [quote, setQuote] = useState(startQuote);
+
+ const handleClick = () => {
+    axios
+    .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+    .then(res => res.data)
+    .then(data => setQuote(data))
+  };
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteCard {...quote[0]}/>
+      <button type="button" onClick={handleClick}>New Quote</button>
     </div>
   );
-}
+};
 
 export default App;
